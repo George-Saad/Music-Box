451b09c4e494bc29f997f6726523e05271ede680
George <george@Georges-MacBook-Pro.local>
2021-09-14 14:20:54 +0200

    Remove the api-key.

--- src/constants.js
+++ src/constants.js
@@ -16,4 +16,4 @@ export const FOREGROUND_CLASS = 'foreground';
 
 export const QUERY_COMPLETE_EVENT = 'query-complete';
 
-export const GIPHY_API_KEY = '2ah9anki4HuLN7g5yJapEXT8fasVqdJu';
+export const GIPHY_API_KEY = '';
